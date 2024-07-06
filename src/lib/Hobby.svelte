f88ec1b3eb53de0d6f40a8df8ab19dfc3f8f28c7
Kamilya Shakirova <94891282+Kamilya05@users.noreply.github.com>
2024-07-06 22:54:59 +0300

    Delete src directory

--- src/lib/Hobby.svelte
+++
@@ -1,9 +0,0 @@
-<div class="hobby">
-    <h1 id="my-hobby">My hobby</h1>
-    <ul>
-        <li><p>puzzles</p></li>
-        <li><p>knotting</p></li>
-        <li><p>volleyball</p></li>
-        <li><p>hiking</p></li>
-    </ul>
-</div>
--- src/lib/Info.svelte
+++
@@ -1,8 +0,0 @@
-<div class="about-me">
-    <div id="about-me">
-        <h1>About me</h1>
-        <p>
-           Innopolis University freshman, olympiad activist, volunteer. I respect all the people around me, ready to help, inquisitive and responsible.
-        </p>
-    </div>
-</div>
