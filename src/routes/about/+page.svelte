1ab4427af36b23ab03d72751c5c3baeb4ed6d755
Kamilya Shakirova <94891282+Kamilya05@users.noreply.github.com>
2024-07-06 22:58:18 +0300

    hw5

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,21 @@
+<div class="main">
+    <div>
+        <div class="about-me">
+			<div id="about-me">
+				<h1>About me</h1>
+				<p>
+				   Innopolis University freshman, olympiad activist, volunteer. I respect all the people around me, ready to help, inquisitive and responsible.
+				</p>
+			</div>
+		</div>
+        <div class="hobby">
+			<h1 id="my-hobby">My hobby</h1>
+			<ul>
+				<li><p>puzzles</p></li>
+				<li><p>knotting</p></li>
+				<li><p>volleyball</p></li>
+				<li><p>hiking</p></li>
+			</ul>
+		</div>
+    </div>
+</div>
---
+++ src/routes/comic/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    import '../../app.css'
+    import {formatDistanceToNowStrict} from "date-fns";
+
+    interface Comic {
+        safe_title: string;
+        img: string;
+        alt: string;
+        year: string;
+        month: string;
+        day: string;
+    }
+    let comicInfo = {
+        comicTitle: 'Loading...',
+        comicImg: 'image/',
+        comicAlt: 'comic',
+        comicDate: '',
+    }
+
+    async function getComic() {
+        const comic_btn = document.getElementById("comic-btn");
+        if (comic_btn != null) {
+            comic_btn.style.setProperty('display', 'none');
+        }
+        const comic_content = document.getElementById("comic-content");
+        if (comic_content != null) {
+            comic_content.style.setProperty('display', 'flex');
+        }
+
+        const email: string = "k.shakirova@innopolis.university";
+        const apiUrl: string = `https://fwd.innopolis.university/api/hw2?email=${email}`;
+
+        fetch(apiUrl)
+            .then((response: Response) => (<PromiseLike<number> | number>response.json()))
+            .then(comicId => {
+                return fetch(`https://fwd.innopolis.university/api/comic?id=${comicId}`);
+            })
+            .then((response: Response) => (<PromiseLike<Comic> | Comic>response.json()))
+            .then(comic => {
+                comicInfo.comicTitle = comic.safe_title;
+                comicInfo.comicImg = comic.img;
+                comicInfo.comicAlt = comic.alt;
+                const date: Date = new Date(Number(comic.year), Number(comic.month) - 1, Number(comic.day));
+                // comicInfo.comicDate = `${date.toLocaleDateString()}`;
+                comicInfo.comicDate = `${date.toLocaleDateString()} - ${formatDistanceToNowStrict(date)}`;
+            })
+    }
+    async function returnComic() {
+        const comic_btn = document.getElementById("comic-btn");
+        if (comic_btn != null) {
+            comic_btn.style.setProperty('display', 'block');
+        }
+        const comic_content = document.getElementById("comic-content");
+        if (comic_content != null) {
+            comic_content.style.setProperty('display', 'none');
+        }
+    }
+
+</script>
+
+<div class="comic">
+    <button id="comic-btn" on:click={async () => await getComic()}>Get a comic!</button>
+    <div id="comic-content">
+        <h1 id="comic-head">Comic</h1>
+        <h2 id="title">{comicInfo.comicTitle}</h2>
+        <img id="image" src="{comicInfo.comicImg}" alt="{comicInfo.comicAlt}">
+        <p id="data">{comicInfo.comicDate}</p>
+        <button id="comic-return" on:click={async () => await returnComic()}>Return</button>
+    </div>
+</div>
+
